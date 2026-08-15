8f56b43df76f1cd7f579e47555da4fd77140ca59
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:09:51 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -17,8 +17,6 @@ export DEPLOY_VULKAN=1
 quick-sharun ./AppDir/bin/SnowboardKids2Recompiled
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
